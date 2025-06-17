4e1278697ba1e4e548832f481c2b5db78f1236c0
Mariya Podosinova <mariya.podosinova@gmail.com>
2025-06-17 10:37:38 +0100

    initial settup

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,44 @@
 import js from "@eslint/js";
 import globals from "globals";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default [
+  /* Ignore build artefacts */
   { ignores: ["dist"] },
+
+  /* React + TypeScript rules */
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
+      sourceType: "module",
       globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
     },
+
     plugins: {
+      react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
+
     rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommendedTypeChecked.rules,
+      ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+
+      /* React 17+ automatic runtime */
+      "react/react-in-jsx-scope": "off",
+      /* Keep your refresh rule */
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
     },
-  }
-);
+  },
+];
