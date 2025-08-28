133df0984979ba2a0b4145a6df7a26cc13411f64
Mariya Podosinova <mariya.podosinova@gmail.com>
2025-08-28 21:23:33 +0100

    fix(styles/build): make custom-media tokens work (global CSS + PostCSS); refactor Notification to use --sb-bp-* with md/lg tweaks; refresh yarn.lock

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,14 @@
 import postcssPresetEnv from "postcss-preset-env";
+import postcssGlobalData from "@csstools/postcss-global-data";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const mediaFile = resolve(__dirname, "src/styles/media.tokens.css");
 
 export default {
   plugins: [
+    postcssGlobalData({ files: [mediaFile] }),
     postcssPresetEnv({
       stage: 3,
       features: { "custom-media-queries": true },
